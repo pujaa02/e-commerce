41f652270af0fb25892be77e7989262f426632da
puja.morvadiya <puja.moravadiya2024@gmail.com>
2024-06-19 18:12:30 +0530

    changes in card.tsx

--- my-app/src/Components/homepage/Cart.tsx
+++ my-app/src/Components/homepage/Cart.tsx
@@ -1,78 +1,3 @@
-import React from "react";
-import "./cart.css";
-// import { useDispatch, useSelector } from "react-redux";
-import { useSelector } from "react-redux";
-import { Grid } from "@mui/material";
-import Header from "./header";
-// import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
-// import AddIcon from '@mui/icons-material/Add';
-// import RemoveIcon from '@mui/icons-material/Remove';
-// import { removeFromCart } from "./CartFuncationality";
-// import { State, Data } from "../interfacefile";
-import { State } from "../interfacefile";
-
-const Cart: React.FC = () => {
-    const cart = useSelector((state: State) => state.cart); // Corrected here
-    // const dispatch = useDispatch();
-    console.log(cart, "cart");
-
-    return (
-        <Grid container sx={{ color: 'text.primary' }}>
-            <div className="home_container">
-                <Header />
-                <header>
-                    <h2 id="header_h2"> Your Cart ( items)</h2>
-                </header>
-                <div className="cart_container">
-                    <table id="cart_item">
-                        <thead>
-                            <tr>
-                                <th>Item</th>
-                                <th>Price</th>
-                                <th>Quantity</th>
-                                <th>Total</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {/* {cart.map((dataCart: Data, index: number) => (
-                                <tr key={index}>
-                                    <td className="flex_items">
-                                        <img id="logo" src={require(`../homepage/mylogo.jpg`)} alt="" />
-                                        <p>pi pizza oven</p>
-                                    </td>
-                                    <td>$6.99</td>
-                                    <td>
-                                        <table className="minitable">
-                                            <tbody>
-                                                <tr>
-                                                    <td className="minitable_td"><RemoveIcon /></td>
-                                                    <td className="minitable_td" width={50}>4</td>
-                                                    <td className="minitable_td"><AddIcon /></td>
-                                                </tr>
-                                            </tbody>
-                                        </table>
-                                    </td>
-                                    <td>$6.99</td>
-                                    <td onClick={() => dispatch(removeFromCart(dataCart.id))}>
-                                        <DeleteForeverIcon className="material_icon" />
-                                    </td>
-                                </tr>
-                            ))} */}
-                        </tbody>
-                    </table>
-                    <div className="subtotal_container">
-                        <p>SubTotal: </p>
-                        <p className="checkout_btn">Checkout</p>
-                    </div>
-                </div>
-            </div>
-        </Grid>
-    );
-};
-
-export default Cart;
-
-
 import { createSlice } from "@reduxjs/toolkit";
 import { toast } from "react-hot-toast";
 import { State } from "../interfacefile";
@@ -94,16 +19,34 @@ const cartSlice = createSlice({
     initialState,
     reducers: {
         addToCart: (state, action) => {
-            const item = action.payload
-            state.cart.push(item)
-            state.totalItems++
-            state.total += item.price
-            localStorage.setItem("cart", JSON.stringify(state.cart))
-            localStorage.setItem("total", JSON.stringify(state.total))
-            localStorage.setItem("totalItems", JSON.stringify(state.totalItems))
-            toast.success("Item added to cart")
+            const itemInCart = state.cart.find((item) => item.product_data_id === action.payload.id);
+            if (itemInCart) {
+                itemInCart.count++;
+            } else {
+                const item = action.payload
+                state.cart.push({ ...item, count: 1 })
+                state.totalItems++
+                state.total += item.price
+                localStorage.setItem("cart", JSON.stringify(state.cart))
+                localStorage.setItem("total", JSON.stringify(state.total))
+                localStorage.setItem("totalItems", JSON.stringify(state.totalItems))
+                toast.success("Item added to cart")
+            }
         },
 
+        incrementQuantity: (state, action) => {
+            const item = state.cart.find((item) => item.product_data_id === action.payload);
+            item.count++;
+
+        },
+        decrementQuantity: (state, action) => {
+            const item = state.cart.find((item) => item.product_data_id === action.payload);
+            if (item.count === 1) {
+                item.count = 1
+            } else {
+                item.count--;
+            }
+        },
         removeFromCart: (state, action) => {
             const itemId = action.payload
             const index = state.cart.findIndex((item: { product_data_id: number; }) => item.product_data_id === itemId)
